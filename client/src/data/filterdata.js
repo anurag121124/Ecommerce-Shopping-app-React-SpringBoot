f1cb5ca67ffaebae356f1ad1275eea7f6402d65b
Anurag Singh <anuragsingh121124@gmail.com>
2023-08-30 18:48:00 +0530

    Filter data

--- client/src/data/filterdata.js
+++ client/src/data/filterdata.js
@@ -38,13 +38,13 @@ export const filters = [
 export const singleFilter = [
   {
     id: "price",
-    name: "price",
+    name: "Price",
     options: [
-      { value: "159-399", label: "$159-$399" },
-      { value: "399-999", label: "$399-$999" },
-      { value: "999-1999", label: "$999-$1999" },
-      { value: "1999-2999", label: "$1999-$2999" },
-      { value: "3999-4999", label: "$3999-$4999" },
+      { value: "159-399", label: "₹159-₹399" },
+      { value: "399-999", label: "₹399-₹999" },
+      { value: "999-1999", label: "₹999-₹1999" },
+      { value: "1999-2999", label: "₹1999-₹2999" },
+      { value: "3999-4999", label: "₹3999-₹4999" },
     ],
   },
   {
