d250d2ab88863f3a141a1745a30735f7ac93f50a
Anurag Singh <anuragsingh121124@gmail.com>
2023-08-30 18:46:33 +0530

    Prouductpage with Filter

---
+++ client/src/data/filterdata.js
@@ -0,0 +1,77 @@
+export const color = [
+  "white",
+  "black",
+  "Red",
+  "marun",
+  "Being",
+  "Pink",
+  "Green",
+  "Yellow",
+];
+
+export const filters = [
+  {
+    id: "color",
+    name: "color",
+    options: [
+      { value: "white", label: "white" },
+      { value: "black", label: "black" },
+      { value: "Red", label: "marun" },
+      { value: "Being", label: "Being" },
+      { value: "Pink", label: "Pink" },
+      { value: "Green", label: "Green" },
+      { value: "Yellow", label: "Yellow" },
+    ],
+  },
+  {
+    id: "size",
+    name: "size",
+    options: [
+      { value: "S", label: "S" },
+      { value: "M", label: "M" },
+      { value: "L", label: "L" },
+      { value: "XL", label: "XL" },
+    ],
+  },
+];
+
+export const singleFilter = [
+  {
+    id: "price",
+    name: "price",
+    options: [
+      { value: "159-399", label: "$159-$399" },
+      { value: "399-999", label: "$399-$999" },
+      { value: "999-1999", label: "$999-$1999" },
+      { value: "1999-2999", label: "$1999-$2999" },
+      { value: "3999-4999", label: "$3999-$4999" },
+    ],
+  },
+  {
+    id: "discount",
+    name: "Disount Range",
+    options: [
+      { value: "10", label: "20% Above" },
+      { value: "20", label: "20% Above" },
+      { value: "30", label: "30% Above" },
+      { value: "40", label: "40% Above" },
+      { value: "50", label: "50% Above" },
+      { value: "60", label: "60% Above" },
+      { value: "70", label: "70% Above" },
+      { value: "80", label: "80% Above" },
+    ],
+  },
+  {
+    id: "stock",
+    name: "Avialability",
+    options: [
+      { value: "in_stock", label: "In Stock" },
+      { value: "out_of_stock", label: "Out of stock" },
+    ],
+  },
+];
+
+export const sortOptions = [
+  { name: "Price: Low To High", query: "price_low", current: false },
+  { name: "Price: High to low", query: "price_high", current: false },
+];
---
+++ client/src/data/mens_kurta.js
@@ -0,0 +1,61 @@
+export  const productData = [
+    {
+      id: 1,
+      imageUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/r/a/c/-original-imagg44kreytceyz.jpeg?q=70',
+      title: 'NoFilter',
+      description: 'Men Printed Pure Cotton Straight Kurta',
+      price: 39.99,
+    },
+    {
+        id: 2,
+        imageUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/r/a/c/-original-imagg44kreytceyz.jpeg?q=70',
+        title: 'NoFilter',
+        description: 'Men Printed Pure Cotton Straight Kurta',
+        price: 39.99,
+      },
+      {
+        id: 3,
+        imageUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/r/a/c/-original-imagg44kreytceyz.jpeg?q=70',
+        title: 'Anurag Singh',
+        description: 'Men Printed Pure Cotton Straight Kurta',
+        price: 39.99,
+      },
+      {
+        id: 4,
+        imageUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/r/a/c/-original-imagg44kreytceyz.jpeg?q=70',
+        title: 'NoFilter',
+        description: 'Men Printed Pure Cotton Straight Kurta',
+        price: 39.99,
+      },
+      {
+        id: 5,
+        imageUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/r/a/c/-original-imagg44kreytceyz.jpeg?q=70',
+        title: 'NoFilter',
+        description: 'Men Printed Pure Cotton Straight Kurta',
+        price: 39.99,
+      },
+      {
+        id: 6,
+        imageUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/r/a/c/-original-imagg44kreytceyz.jpeg?q=70',
+        title: 'NoFilter',
+        description: 'Men Printed Pure Cotton Straight Kurta',
+        price: 39.99,
+      },
+      {
+        id: 7,
+        imageUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/r/a/c/-original-imagg44kreytceyz.jpeg?q=70',
+        title: 'NoFilter',
+        description: 'Men Printed Pure Cotton Straight Kurta',
+        price: 39.99,
+      },
+      {
+        id: 8,
+        imageUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/r/a/c/-original-imagg44kreytceyz.jpeg?q=70',
+        title: 'NoFilter',
+        description: 'Men Printed Pure Cotton Straight Kurta',
+        price: 39.99,
+      },
+    
+    // Add more product objects
+  ];
+  
