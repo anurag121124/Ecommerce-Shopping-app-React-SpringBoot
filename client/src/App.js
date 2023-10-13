0cb66462a52e35499387cfca5b4c94f356a66643
Anurag Singh <anuragsingh121124@gmail.com>
2023-10-13 10:38:20 +0530

    send

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,15 @@ import Navigation from './customer/components/Header/Navbar';
 import Homepage from './customer/Pages/Homepage/Homepage';
 import Footer from './customer/components/Footer/footer.jsx';
 import Product from './customer/components/Product/Product';
+import Productdetails from './customer/components/ProductDetails/Productdetails';
 
 function App() {
   return (
    <div className="">
    <Navigation/>
    <div>
- 
- <Homepage></Homepage>
+  {/* <Product/> */}
+  <Homepage/>
     </div>
     <Footer/>
     </div>
